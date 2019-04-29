c95d0538146835deef57f854233d74028beb91a8
Anderswith <32036127+Anderswith@users.noreply.github.com>
2019-04-29 16:34:20 +0200

    update
    custom lavede knapper + funktionalitet og effekt
    åbner rette view

--- src/quickmaff_belman/gui/controller/LoginController.java
+++ src/quickmaff_belman/gui/controller/LoginController.java
@@ -5,8 +5,8 @@
  */
 package quickmaff_belman.gui.controller;
 
+import static java.awt.Color.RED;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -18,16 +18,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
+import static javafx.scene.paint.Color.color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.json.simple.parser.ParseException;
@@ -100,55 +105,58 @@ public class LoginController implements Initializable {
 
     public ObservableList<String> getDepartmentNames() {
 
-        dep.add("a");
-        dep.add("b");
-        dep.add("c");
-        dep.add("d");
-        dep.add("e");
-        dep.add("f");
-        dep.add("g");
-        dep.add("h");
-        dep.add("i");
-        dep.add("j");
-        dep.add("k");
-        dep.add("l");
-        dep.add("m");
-        dep.add("n");
-        dep.add("o");
-        dep.add("p");
-        dep.add("q");
-        dep.add("r");
+        dep.add("Department 1");
+        dep.add("Department 2");
+        dep.add("Department 3");
+        dep.add("Department 4");
+        dep.add("Department 5");
+        dep.add("Department 6");
+        dep.add("Department 7");
+
 
         return dep;
     }
 
-    public void buttonGenerator() {
+    public void loadGrid() {
 
         ObservableList<String> depNames = getDepartmentNames();
 
         int i = 0; //column index
         int j = 0; //row index
-
-        for (String buttonName : depNames) {
-            Button newButton = new Button(buttonName);
-            newButton.setBorder(Border.EMPTY);
-            newButton.setPrefHeight(25);
-            newButton.setPrefWidth(200);
+        pane.setStyle("-fx-background-color: RED;");
+        
+        for (String depName : depNames) {
+//            Image image = new Image("/quickmaff_belman/gui/view/images/ButtonOFF.png");
+//            Label lblDepName = new Label(depName);
+            
+            Button newButton = new Button(depName);
+//            newButton.backgroundProperty().setValue(Background.getClassCssMetaData().addAll(image));
+//            newButton.
+//            newButton.setBackground(Background.EMPTY);
+//            newButton.setGraphic(new ImageView(image));
+            newButton.setPrefHeight(203);
+            newButton.setPrefWidth(206);
+//            newButton.setb
+            newButton.setStyle("-fx-background-image: url(\"/quickmaff_belman/gui/view/images/ButtonOFF.png\");");
+//            newButton.setStyle("-fx-background-repeat: stretch;");
+//            newButton.setStyle("-fx-background-size: 100 50;");
+            newButton.scaleShapeProperty().setValue(Boolean.TRUE);
+            newButton.autosize();
             //adds mouse clicked event to the button
             newButton.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, e -> {
-                try {
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/quickmaff_belman/gui/view/Authentication.fxml"));
-                    Parent root = (Parent) fxmlLoader.load();
-                    AuthenticationController hc = fxmlLoader.getController();
-                    Stage stage = new Stage();
-                    stage.setScene(new Scene(root));
-                    stage.show();
-                } catch (IOException ex) {
-                    Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                newButton.setStyle("-fx-background-image: url(\"/quickmaff_belman/gui/view/images/ButtonON.png\");");
+                openMainView();
+                
             });
             //adds button to gridpane with coordinates
+            
             gridPane.add(newButton, j, i);
+            
+            
+            
+//            gridPane.add(new ImageView(image), j, i);
+            
+//            gridPane.setStyle("-fx-background-color: transparent;");
             //makes sure to get the right coordinates for column and row.
             j++;
             if (j == 2) {
@@ -156,6 +164,27 @@ public class LoginController implements Initializable {
                 i++;
             }
         }
+        
     }
+    
+    public void openMainView(){
+        
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/quickmaff_belman/gui/view/MainView.fxml"));
+            Parent root = loader.load();
+            MainViewController con = loader.getController();
+            con.setModel(new Model(new BLLManager(new DatabaseFacade())));
+            con.initView();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception ex) {
+            ExceptionHandler.handleException(ex);
+        }
+        
+        
+    }
+
 
 }
