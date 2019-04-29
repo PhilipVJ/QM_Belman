5479a1fb91b3185ae9b51acd6f218b51eb886ff4
PhilipV <44639834+PhilipVJ@users.noreply.github.com>
2019-04-29 15:48:06 +0200

    tavle

--- src/quickmaff_belman/gui/controller/MainViewController.java
+++ src/quickmaff_belman/gui/controller/MainViewController.java
@@ -5,15 +5,34 @@
  */
 package quickmaff_belman.gui.controller;
 
+import com.jfoenix.controls.JFXScrollPane;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import quickmaff_belman.gui.model.Language;
 import quickmaff_belman.gui.model.Model;
 
@@ -31,13 +50,18 @@ public class MainViewController implements Initializable {
     private Label department;
     @FXML
     private BorderPane borderPane;
+    @FXML
+    private GridPane grid;
+    @FXML
+    private AnchorPane anchorPane;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-//        borderPane.getCenter().setStyle("-fx-background-color: #FFA435");
+
+        anchorPane.setPrefHeight(6000);
 
     }
 
@@ -67,6 +91,37 @@ public class MainViewController implements Initializable {
 
     public void initView() {
         setAllText();
+        testScroll();
+    }
+
+    public void testScroll() {
+        ArrayList<String> names = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            names.add("Button: " + i);
+        }
+
+        int i = 0; //column index
+        int j = 0; //row index
+
+        for (String buttonName : names) {
+            Image daImage = new Image("/quickmaff_belman/gui/view/images/postit2.png");
+            ImageView view = new ImageView(daImage);
+
+            view.setPreserveRatio(true);
+            view.setFitWidth(160);
+            view.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, e -> {
+                System.out.println("Opening task " + buttonName);
+            });
+            HBox box = new HBox(view);
+            box.setAlignment(Pos.CENTER);
+            grid.add(box, j, i);
+            j++;
+            if (j == 3) {
+                j = 0;
+                i++;
+            }
+        }
+
     }
 
     private void setAllText() {
