087d82b2615d3da9165d565f91ef899da94e65c0
Neller1989 <43983250+Neller1989@users.noreply.github.com>
2019-05-27 12:32:39 +0200

    procent i progressbar

--- src/quickmaff_belman/gui/model/BoardMaker.java
+++ src/quickmaff_belman/gui/model/BoardMaker.java
@@ -248,9 +248,18 @@ public class BoardMaker implements Runnable {
         progressPane.setTranslateX(1150);
         progressPane.setTranslateY(200);
         progressPane.setPrefHeight(250);
-        progressPane.setPrefWidth(180);
+        progressPane.setPrefWidth(180); 
+        
+        Label showPercantage = new Label();
+        showPercantage.setTranslateX(-1345);
+        showPercantage.setTranslateY(50);
+        Double value = Utility.getPercentageTimeLeft(bTask) * 100;
+        String percentageSub = Double.toString(value);
+        int lastIndexOfDot = percentageSub.lastIndexOf(".");
+        showPercantage.setText(percentageSub.substring(0, lastIndexOfDot + 2) + " %");
+        
         Label lbl = labelMaker.makeLabelForProgressBar("estimateTime", 26, -1410);
-        double percantage = Utility.getPercentageTimeLeft(bTask);
+        double percantage = Utility.getPercentageTimeLeft(bTask); 
         ProgressBar pBar = new ProgressBar();
         pBar.setProgress(percantage);
 
@@ -258,7 +267,7 @@ public class BoardMaker implements Runnable {
         pBar.setPrefWidth(250);
         pBar.setTranslateX(-1350);
         pBar.setTranslateY(50);
-        progressPane.getChildren().addAll(pBar, lbl);
+        progressPane.getChildren().addAll(pBar, lbl, showPercantage);
         return progressPane;
     }
 
